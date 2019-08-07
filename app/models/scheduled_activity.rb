7140c7f957d60069737d887e09f0f6456d106306
nick stern <nicholas.stern2@gmail.com>
2019-08-07 12:26:11 +0800

    Add dotenv - Protect my secret data in .env file

--- app/models/scheduled_activity.rb
+++ app/models/scheduled_activity.rb
@@ -1,4 +1,7 @@
 class ScheduledActivity < ApplicationRecord
+ geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
   belongs_to :user
   belongs_to :activity_type
   # has :name, through: :activity_type
