218a935da162062cb1290b94be4c630ee02854fc
nick stern <nicholas.stern2@gmail.com>
2019-08-08 12:20:11 +0800

    scheduled show page fix

--- app/controllers/scheduled_activities_controller.rb
+++ app/controllers/scheduled_activities_controller.rb
@@ -11,7 +11,7 @@ class ScheduledActivitiesController < ApplicationController
   end
 
   def show
-    @activity_type = ActivityType.find(params[:id])
+    # @activity_type = ActivityType.find(params[:id])
 
     @scheduled_activity = ScheduledActivity.find(params[:id])
     @scheduled_activities = ScheduledActivity.where(activity_type_id: params[:activity_type])
